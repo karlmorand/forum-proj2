fbb9f0b782a1da817a697e9682185e76af59a739
karlmorand <karljmorand@gmail.com>
2016-06-24 19:15:55 -0400

    post and user controllers connected and tested for /users and /posts routes

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 var express = require('express');
 var app = express();
-var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
 var userController = require('./controllers/userController.js');
 var postController = require('./controllers/postController.js');
 
-app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/users', userController);
-app.use('/postController', postController);
+app.use('/posts', postController);
 
 
 app.get('/', function(req, res){
