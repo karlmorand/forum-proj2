adff16b0855163cf6f7a12e5c28663063ae8799d
Karl Morand <karljmorand@gmail.com>
2016-06-25 22:38:10 -0400

    Added route to display signup page

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ app.use('/posts', postController);
 app.get('/login', function(req, res){//eventually will need to update this to redirect to posts index if user is already logged in (tested by checking session data)
   res.render('login.html.ejs');
 })
+app.get('/signup', function(req, res){
+	res.render('signup.html.ejs');
+	});
 
 app.get('/', function(req, res){
   res.render('home.html.ejs')
